45e32b38d69fc0b532feb2d9a8f4b93d2d196116
Amiel <amieljames.delatorre@gmail.com>
2020-10-08 04:24:00 +1300

    slight changes to appearance and some functionality

--- flix/home/home.py
+++ flix/home/home.py
@@ -1,4 +1,5 @@
 from flask import Blueprint, render_template
+from flask import request, redirect, url_for, session
 import flix.utilities.utilities as utilities
 import flix.utilities.services as services
 import flix.adapters.repository as repo
@@ -8,7 +9,9 @@ home_blueprint = Blueprint('home_bp', __name__)
 @home_blueprint.route('/', methods=['GET'])
 def home():
     movies = services.get_random_movies(4, repo.repo_instance)
+    refresh_url = url_for('home_bp.home')
     # print(movies)
     return render_template('home/home.html',
-                           movies=movies
+                           movies=movies,
+                           refresh_url=refresh_url
                            )
